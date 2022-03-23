595394faaeeb9d56bcd3b5fbf96e4f9ed0fe349e
Geverson Souza <geversonjosedesouza@gmail.com>
2022-03-22 23:10:21 -0300

    Update alterar_senha_mysql.sh

--- alterar_senha_mysql.sh
+++ alterar_senha_mysql.sh
@@ -1,9 +1,1 @@
-sudo systemctl stop mysql.service
-sudo mkdir -p /var/run/mysqld
-sudo chown mysql:mysql /var/run/mysqld
-#Em um novo terminal
-sudo mysqld_safe --skip-grant-tables --skip-networking
-mysql -u root
-update mysql.user set authentication_string='12345678' where user='root';
-flush privileges;
-exit;
+ sudo grep 'password' /var/log/mysqld.log
